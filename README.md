3b6f213e3f41a52c366fbab036b8d7e647ae3ace
Alessio Garofalo <alessio@linux.com>
2019-09-28 17:53:17 +0100

    helm fixes

---
+++ README.md
@@ -0,0 +1,101 @@
+[![CircleCI](https://circleci.com/gh/giantswarm/kubernetes-elastic-stack.svg?style=shield)]()
+
+# Honeypot stack in kubernetes
+
+See [docs](docs/index.md) for full recipe content.
+
+
+This setup is similar to the [`Docker-compose Stack Example`](), but adopted to be run on a Kubernetes cluster.
+
+There is no access control for the Kibana web interface. If you want to run this in public you need to secure your setup. The provided manifests here are for demonstration purposes only.
+
+## Honeypots
+
+* [adbhoney](https://github.com/huuck/ADBHoney),
+* [ciscoasa](https://github.com/Cymmetria/ciscoasa_honeypot),
+* [conpot](http://conpot.org/),
+* [cowrie](https://github.com/cowrie/cowrie),
+* [dionaea](https://github.com/DinoTools/dionaea),
+* [elasticpot](https://github.com/schmalle/ElasticpotPY),
+* [glutton](https://github.com/mushorg/glutton),
+* [heralding](https://github.com/johnnykv/heralding),
+* [honeypy](https://github.com/foospidy/HoneyPy),
+* [honeytrap](https://github.com/armedpot/honeytrap/),
+* [mailoney](https://github.com/awhitehatter/mailoney),
+* [medpot](https://github.com/schmalle/medpot),
+* [rdpy](https://github.com/citronneur/rdpy),
+* [snare](http://mushmush.org/),
+* [tanner](http://mushmush.org/)
+
+
+# Local Node Setup
+
+## Start a local Kubernetes using minikube
+
+> If some webpages don't show up immediately wait a bit and reload. Also the Kubernetes Dashboard needs reloading to update its view.
+
+```bash
+minikube start --memory 4096
+minikube dashboard
+# maybe wait a bit and retry
+kubectl get --all-namespaces services,pods
+```
+
+# Multiple node cluster
+## Storage
+
+### Nfs provisioner
+```
+helm install stable/nfs-server-provisioner nfs-provisioner -n nfs
+```
+
+persistence:
+enabled: true
+storageClass: "-"
+size: 200Gi
+
+storageClass:
+defaultClass: true
+
+nodeSelector:
+kubernetes.io/hostname: {node-name}
+
+In this configuration, a PersistentVolume must be created for each replica to use. Installing the Helm chart, and then inspecting the PersistentVolumeClaim's created will provide the necessary names for your PersistentVolume's to bind to.
+
+An example of the necessary PersistentVolume:
+
+```
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+name: data-nfs-server-provisioner-0
+spec:
+capacity:
+    storage: 200Gi
+accessModes:
+    - ReadWriteOnce
+hostPath:
+    path: /srv/volumes/data-nfs-server-provisioner-0
+claimRef:
+    namespace: nfs
+    name: data-nfs-server-provisioner-0
+```
+
+## Logging with Elasticsearch and Kibana
+
+```bash
+helm install -n bee elasticsearch .
+helm install -n bee kibana .
+helm install -n bee ewsposter .
+```
+
+
+
+To delete the whole local Kubernetes cluster use this:
+
+```bash
+minikube delete
+```
+
+# Credits
+TpotCE [githu.com/dtag-dev-sec/tpotce]
--- bin/clean.sh
+++ bin/clean.sh
@@ -2,40 +2,40 @@
 # T-Pot Container Data Cleaner & Log Rotator
 
 # Set colors
-myRED="[0;31m"
-myGREEN="[0;32m"
-myWHITE="[0;0m"
+RED="[0;31m"
+GREEN="[0;32m"
+WHITE="[0;0m"
 
 # Set persistence
-myPERSISTENCE=$1
+PERSISTENCE=$1
 
-# Let's create a function to check if folder is empty
-fuEMPTY() {
-  local myFOLDER=$1
+# Let's create a nction to check if folder is empty
+EMPTY() {
+  local FOLDER=$1
 
-  echo $(ls $myFOLDER | wc -l)
+  echo $(ls $FOLDER | wc -l)
 }
 
-# Let's create a function to rotate and compress logs
-fuLOGROTATE() {
-  local mySTATUS="/opt/beehive/etc/logrotate/status"
-  local myCONF="/opt/beehive/etc/logrotate/logrotate.conf"
-  local myADBHONEYTGZ="/data/adbhoney/downloads.tgz"
-  local myADBHONEYDL="/data/adbhoney/downloads/"
-  local myCOWRIETTYLOGS="/data/cowrie/log/tty/"
-  local myCOWRIETTYTGZ="/data/cowrie/log/ttylogs.tgz"
-  local myCOWRIEDL="/data/cowrie/downloads/"
-  local myCOWRIEDLTGZ="/data/cowrie/downloads.tgz"
-  local myDIONAEABI="/data/dionaea/bistreams/"
-  local myDIONAEABITGZ="/data/dionaea/bistreams.tgz"
-  local myDIONAEABIN="/data/dionaea/binaries/"
-  local myDIONAEABINTGZ="/data/dionaea/binaries.tgz"
-  local myHONEYTRAPATTACKS="/data/honeytrap/attacks/"
-  local myHONEYTRAPATTACKSTGZ="/data/honeytrap/attacks.tgz"
-  local myHONEYTRAPDL="/data/honeytrap/downloads/"
-  local myHONEYTRAPDLTGZ="/data/honeytrap/downloads.tgz"
-  local myTANNERF="/data/tanner/files/"
-  local myTANNERFTGZ="/data/tanner/files.tgz"
+# Let's create a nction to rotate and compress logs
+LOGROTATE() {
+  local STATUS="/opt/beehive/etc/logrotate/status"
+  local CONF="/opt/beehive/etc/logrotate/logrotate.conf"
+  local ADBHONEYTGZ="/data/adbhoney/downloads.tgz"
+  local ADBHONEYDL="/data/adbhoney/downloads/"
+  local COWRIETTYLOGS="/data/cowrie/log/tty/"
+  local COWRIETTYTGZ="/data/cowrie/log/ttylogs.tgz"
+  local COWRIEDL="/data/cowrie/downloads/"
+  local COWRIEDLTGZ="/data/cowrie/downloads.tgz"
+  local DIONAEABI="/data/dionaea/bistreams/"
+  local DIONAEABITGZ="/data/dionaea/bistreams.tgz"
+  local DIONAEABIN="/data/dionaea/binaries/"
+  local DIONAEABINTGZ="/data/dionaea/binaries.tgz"
+  local HONEYTRAPATTACKS="/data/honeytrap/attacks/"
+  local HONEYTRAPATTACKSTGZ="/data/honeytrap/attacks.tgz"
+  local HONEYTRAPDL="/data/honeytrap/downloads/"
+  local HONEYTRAPDLTGZ="/data/honeytrap/downloads.tgz"
+  local TANNERF="/data/tanner/files/"
+  local TANNERFTGZ="/data/tanner/files.tgz"
 
   # Ensure correct permissions and ownerships for logrotate to run without issues
   chmod 760 /data/ -R
@@ -44,223 +44,223 @@ fuLOGROTATE() {
   chmod 644 /data/nginx/cert -R
 
   # Run logrotate with force (-f) first, so the status file can be written and race conditions (with tar) be avoided
-  logrotate -f -s $mySTATUS $myCONF
+  logrotate -f -s $STATUS $CONF
 
   # Compressing some folders first and rotate them later
-  if [ "$(fuEMPTY $myADBHONEYDL)" != "0" ]; then tar cvfz $myADBHONEYTGZ $myADBHONEYDL; fi
-  if [ "$(fuEMPTY $myCOWRIETTYLOGS)" != "0" ]; then tar cvfz $myCOWRIETTYTGZ $myCOWRIETTYLOGS; fi
-  if [ "$(fuEMPTY $myCOWRIEDL)" != "0" ]; then tar cvfz $myCOWRIEDLTGZ $myCOWRIEDL; fi
-  if [ "$(fuEMPTY $myDIONAEABI)" != "0" ]; then tar cvfz $myDIONAEABITGZ $myDIONAEABI; fi
-  if [ "$(fuEMPTY $myDIONAEABIN)" != "0" ]; then tar cvfz $myDIONAEABINTGZ $myDIONAEABIN; fi
-  if [ "$(fuEMPTY $myHONEYTRAPATTACKS)" != "0" ]; then tar cvfz $myHONEYTRAPATTACKSTGZ $myHONEYTRAPATTACKS; fi
-  if [ "$(fuEMPTY $myHONEYTRAPDL)" != "0" ]; then tar cvfz $myHONEYTRAPDLTGZ $myHONEYTRAPDL; fi
-  if [ "$(fuEMPTY $myTANNERF)" != "0" ]; then tar cvfz $myTANNERFTGZ $myTANNERF; fi
+  if [ "$(EMPTY $ADBHONEYDL)" != "0" ]; then tar cvfz $ADBHONEYTGZ $ADBHONEYDL; fi
+  if [ "$(EMPTY $COWRIETTYLOGS)" != "0" ]; then tar cvfz $COWRIETTYTGZ $COWRIETTYLOGS; fi
+  if [ "$(EMPTY $COWRIEDL)" != "0" ]; then tar cvfz $COWRIEDLTGZ $COWRIEDL; fi
+  if [ "$(EMPTY $DIONAEABI)" != "0" ]; then tar cvfz $DIONAEABITGZ $DIONAEABI; fi
+  if [ "$(EMPTY $DIONAEABIN)" != "0" ]; then tar cvfz $DIONAEABINTGZ $DIONAEABIN; fi
+  if [ "$(EMPTY $HONEYTRAPATTACKS)" != "0" ]; then tar cvfz $HONEYTRAPATTACKSTGZ $HONEYTRAPATTACKS; fi
+  if [ "$(EMPTY $HONEYTRAPDL)" != "0" ]; then tar cvfz $HONEYTRAPDLTGZ $HONEYTRAPDL; fi
+  if [ "$(EMPTY $TANNERF)" != "0" ]; then tar cvfz $TANNERFTGZ $TANNERF; fi
 
   # Ensure correct permissions and ownership for previously created archives
-  chmod 760 $myADBHONEYTGZ $myCOWRIETTYTGZ $myCOWRIEDLTGZ $myDIONAEABITGZ $myDIONAEABINTGZ $myHONEYTRAPATTACKSTGZ $myHONEYTRAPDLTGZ $myTANNERFTGZ
-  chown beehive:beehive $myADBHONEYTGZ $myCOWRIETTYTGZ $myCOWRIEDLTGZ $myDIONAEABITGZ $myDIONAEABINTGZ $myHONEYTRAPATTACKSTGZ $myHONEYTRAPDLTGZ $myTANNERFTGZ
+  chmod 760 $ADBHONEYTGZ $COWRIETTYTGZ $COWRIEDLTGZ $DIONAEABITGZ $DIONAEABINTGZ $HONEYTRAPATTACKSTGZ $HONEYTRAPDLTGZ $TANNERFTGZ
+  chown beehive:beehive $ADBHONEYTGZ $COWRIETTYTGZ $COWRIEDLTGZ $DIONAEABITGZ $DIONAEABINTGZ $HONEYTRAPATTACKSTGZ $HONEYTRAPDLTGZ $TANNERFTGZ
 
   # Need to remove subfolders since too many files cause rm to exit with errors
-  rm -rf $myADBHONEYDL $myCOWRIETTYLOGS $myCOWRIEDL $myDIONAEABI $myDIONAEABIN $myHONEYTRAPATTACKS $myHONEYTRAPDL $myTANNERF
+  rm -rf $ADBHONEYDL $COWRIETTYLOGS $COWRIEDL $DIONAEABI $DIONAEABIN $HONEYTRAPATTACKS $HONEYTRAPDL $TANNERF
 
   # Recreate subfolders with correct permissions and ownership
-  mkdir -p $myADBHONEYDL $myCOWRIETTYLOGS $myCOWRIEDL $myDIONAEABI $myDIONAEABIN $myHONEYTRAPATTACKS $myHONEYTRAPDL $myTANNERF
-  chmod 760 $myADBHONEYDL $myCOWRIETTYLOGS $myCOWRIEDL $myDIONAEABI $myDIONAEABIN $myHONEYTRAPATTACKS $myHONEYTRAPDL $myTANNERF
-  chown beehive:beehive $myADBHONEYDL $myCOWRIETTYLOGS $myCOWRIEDL $myDIONAEABI $myDIONAEABIN $myHONEYTRAPATTACKS $myHONEYTRAPDL $myTANNERF
+  mkdir -p $ADBHONEYDL $COWRIETTYLOGS $COWRIEDL $DIONAEABI $DIONAEABIN $HONEYTRAPATTACKS $HONEYTRAPDL $TANNERF
+  chmod 760 $ADBHONEYDL $COWRIETTYLOGS $COWRIEDL $DIONAEABI $DIONAEABIN $HONEYTRAPATTACKS $HONEYTRAPDL $TANNERF
+  chown beehive:beehive $ADBHONEYDL $COWRIETTYLOGS $COWRIEDL $DIONAEABI $DIONAEABIN $HONEYTRAPATTACKS $HONEYTRAPDL $TANNERF
 
   # Run logrotate again to account for previously created archives - DO NOT FORCE HERE!
-  logrotate -s $mySTATUS $myCONF
+  logrotate -s $STATUS $CONF
 }
 
-# Let's create a function to clean up and prepare honeytrap data
-fuADBHONEY() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/adbhoney/*; fi
+# Let's create a nction to clean up and prepare honeytrap data
+ADBHONEY() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/adbhoney/*; fi
   mkdir -p /data/adbhoney/log/ /data/adbhoney/downloads/
   chmod 760 /data/adbhoney/ -R
   chown beehive:beehive /data/adbhoney/ -R
 }
 
-# Let's create a function to clean up and prepare ciscoasa data
-fuCISCOASA() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/ciscoasa/*; fi
+# Let's create a nction to clean up and prepare ciscoasa data
+CISCOASA() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/ciscoasa/*; fi
   mkdir -p /data/ciscoasa/log
   chmod 760 /data/ciscoasa -R
   chown beehive:beehive /data/ciscoasa -R
 }
 
-# Let's create a function to clean up and prepare conpot data
-fuCONPOT() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/conpot/*; fi
+# Let's create a nction to clean up and prepare conpot data
+CONPOT() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/conpot/*; fi
   mkdir -p /data/conpot/log
   chmod 760 /data/conpot -R
   chown beehive:beehive /data/conpot -R
 }
 
-# Let's create a function to clean up and prepare cowrie data
-fuCOWRIE() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/cowrie/*; fi
+# Let's create a nction to clean up and prepare cowrie data
+COWRIE() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/cowrie/*; fi
   mkdir -p /data/cowrie/log/tty/ /data/cowrie/downloads/ /data/cowrie/keys/ /data/cowrie/misc/
   chmod 760 /data/cowrie -R
   chown beehive:beehive /data/cowrie -R
 }
 
-# Let's create a function to clean up and prepare dionaea data
-fuDIONAEA() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/dionaea/*; fi
+# Let's create a nction to clean up and prepare dionaea data
+DIONAEA() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/dionaea/*; fi
   mkdir -p /data/dionaea/log /data/dionaea/bistreams /data/dionaea/binaries /data/dionaea/rtp /data/dionaea/roots/ftp /data/dionaea/roots/tftp /data/dionaea/roots/www /data/dionaea/roots/upnp
   chmod 760 /data/dionaea -R
   chown beehive:beehive /data/dionaea -R
 }
 
-# Let's create a function to clean up and prepare elasticpot data
-fuELASTICPOT() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/elasticpot/*; fi
+# Let's create a nction to clean up and prepare elasticpot data
+ELASTICPOT() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/elasticpot/*; fi
   mkdir -p /data/elasticpot/log
   chmod 760 /data/elasticpot -R
   chown beehive:beehive /data/elasticpot -R
 }
 
-# Let's create a function to clean up and prepare elk data
-fuELK() {
+# Let's create a nction to clean up and prepare elk data
+ELK() {
   # ELK data will be kept for <= 90 days, check /etc/crontab for curator modification
   # ELK daemon log files will be removed
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/elk/log/*; fi
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/elk/log/*; fi
   mkdir -p /data/elk
   chmod 760 /data/elk -R
   chown beehive:beehive /data/elk -R
 }
 
-# Let's create a function to clean up and prepare glastopf data
-fuGLASTOPF() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/glastopf/*; fi
+# Let's create a nction to clean up and prepare glastopf data
+GLASTOPF() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/glastopf/*; fi
   mkdir -p /data/glastopf/db /data/glastopf/log
   chmod 760 /data/glastopf -R
   chown beehive:beehive /data/glastopf -R
 }
 
-# Let's create a function to clean up and prepare glastopf data
-fuGLUTTON() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/glutton/*; fi
+# Let's create a nction to clean up and prepare glastopf data
+GLUTTON() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/glutton/*; fi
   mkdir -p /data/glutton/log
   chmod 760 /data/glutton -R
   chown beehive:beehive /data/glutton -R
 }
 
-# Let's create a function to clean up and prepare heralding data
-fuHERALDING() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/heralding/*; fi
+# Let's create a nction to clean up and prepare heralding data
+HERALDING() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/heralding/*; fi
   mkdir -p /data/heralding/log
   chmod 760 /data/heralding -R
   chown beehive:beehive /data/heralding -R
 }
 
-# Let's create a function to clean up and prepare honeytrap data
-fuHONEYTRAP() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/honeytrap/*; fi
+# Let's create a nction to clean up and prepare honeytrap data
+HONEYTRAP() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/honeytrap/*; fi
   mkdir -p /data/honeytrap/log/ /data/honeytrap/attacks/ /data/honeytrap/downloads/
   chmod 760 /data/honeytrap/ -R
   chown beehive:beehive /data/honeytrap/ -R
 }
 
-# Let's create a function to clean up and prepare mailoney data
-fuMAILONEY() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/mailoney/*; fi
+# Let's create a nction to clean up and prepare mailoney data
+MAILONEY() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/mailoney/*; fi
   mkdir -p /data/mailoney/log/
   chmod 760 /data/mailoney/ -R
   chown beehive:beehive /data/mailoney/ -R
 }
 
-# Let's create a function to clean up and prepare mailoney data
-fuMEDPOT() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/medpot/*; fi
+# Let's create a nction to clean up and prepare mailoney data
+MEDPOT() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/medpot/*; fi
   mkdir -p /data/medpot/log/
   chmod 760 /data/medpot/ -R
   chown beehive:beehive /data/medpot/ -R
 }
 
-# Let's create a function to clean up nginx logs
-fuNGINX() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/nginx/log/*; fi
+# Let's create a nction to clean up nginx logs
+NGINX() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/nginx/log/*; fi
   touch /data/nginx/log/error.log
   chmod 644 /data/nginx/conf -R
   chmod 644 /data/nginx/cert -R
 }
 
-# Let's create a function to clean up and prepare rdpy data
-fuRDPY() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/rdpy/*; fi
+# Let's create a nction to clean up and prepare rdpy data
+RDPY() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/rdpy/*; fi
   mkdir -p /data/rdpy/log/
   chmod 760 /data/rdpy/ -R
   chown beehive:beehive /data/rdpy/ -R
 }
 
-# Let's create a function to prepare spiderfoot db
-fuSPIDERFOOT() {
+# Let's create a nction to prepare spiderfoot db
+SPIDERFOOT() {
   mkdir -p /data/spiderfoot
   touch /data/spiderfoot/spiderfoot.db
   chmod 760 -R /data/spiderfoot
   chown beehive:beehive -R /data/spiderfoot
 }
 
-# Let's create a function to clean up and prepare suricata data
-fuSURICATA() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/suricata/*; fi
+# Let's create a nction to clean up and prepare suricata data
+SURICATA() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/suricata/*; fi
   mkdir -p /data/suricata/log
   chmod 760 -R /data/suricata
   chown beehive:beehive -R /data/suricata
 }
 
-# Let's create a function to clean up and prepare p0f data
-fuP0F() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/p0f/*; fi
+# Let's create a nction to clean up and prepare p0f data
+P0F() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/p0f/*; fi
   mkdir -p /data/p0f/log
   chmod 760 -R /data/p0f
   chown beehive:beehive -R /data/p0f
 }
 
-# Let's create a function to clean up and prepare p0f data
-fuTANNER() {
-  if [ "$myPERSISTENCE" != "on" ]; then rm -rf /data/tanner/*; fi
+# Let's create a nction to clean up and prepare p0f data
+TANNER() {
+  if [ "$PERSISTENCE" != "on" ]; then rm -rf /data/tanner/*; fi
   mkdir -p /data/tanner/log /data/tanner/files
   chmod 760 -R /data/tanner
   chown beehive:beehive -R /data/tanner
 }
 
 # Avoid unwanted cleaning
-if [ "$myPERSISTENCE" = "" ]; then
-  echo $myRED"!!! WARNING !!! - This will delete ALL honeypot logs. "$myWHITE
-  while [ "$myQST" != "y" ] && [ "$myQST" != "n" ]; do
-    read -p "Continue? (y/n) " myQST
+if [ "$PERSISTENCE" = "" ]; then
+  echo $RED"!!! WARNING !!! - This will delete ALL honeypot logs. "$WHITE
+  while [ "$QST" != "y" ] && [ "$QST" != "n" ]; do
+    read -p "Continue? (y/n) " QST
   done
-  if [ "$myQST" = "n" ]; then
-    echo $myGREEN"Puuh! That was close! Aborting!"$myWHITE
+  if [ "$QST" = "n" ]; then
+    echo $GREEN"Puuh! That was close! Aborting!"$WHITE
     exit
   fi
 fi
 
 # Check persistence, if enabled compress and rotate logs
-if [ "$myPERSISTENCE" = "on" ]; then
+if [ "$PERSISTENCE" = "on" ]; then
   echo "Persistence enabled, now rotating and compressing logs."
-  fuLOGROTATE
+  LOGROTATE
 else
   echo "Cleaning up and preparing data folders."
-  fuADBHONEY
-  fuCISCOASA
-  fuCONPOT
-  fuCOWRIE
-  fuDIONAEA
-  fuELASTICPOT
-  fuELK
-  fuGLASTOPF
-  fuGLUTTON
-  fuHERALDING
-  fuHONEYTRAP
-  fuMAILONEY
-  fuMEDPOT
-  fuNGINX
-  fuRDPY
-  fuSPIDERFOOT
-  fuSURICATA
-  fuP0F
-  fuTANNER
+  ADBHONEY
+  CISCOASA
+  CONPOT
+  COWRIE
+  DIONAEA
+  ELASTICPOT
+  ELK
+  GLASTOPF
+  GLUTTON
+  HERALDING
+  HONEYTRAP
+  MAILONEY
+  MEDPOT
+  NGINX
+  RDPY
+  SPIDERFOOT
+  SURICATA
+  P0F
+  TANNER
 fi
